9d7353db953570ecde4d27b9814f18e7c6e78b34
vanyap1 <princ.va@gmail.com>
2025-06-24 22:08:50 +0300

    added graphs and analog meters

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scatterlayout import ScatterLayout
 from kivy.uix.screenmanager import Screen , ScreenManager
 from threading import Thread
 from kivy.clock import Clock
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.image import Image
-from kivy.uix.scatter import Scatter
 from kivy.properties import StringProperty, NumericProperty, BooleanProperty, ObjectProperty, ListProperty
 from datetime import datetime, date, timedelta
 from collections import namedtuple
@@ -26,12 +26,16 @@ from kivy.config import Config
 from kivy.core.window import Window
 from kivy.factory import Factory
 from kivy.uix.actionbar import ActionBar
+from kivy.utils import get_color_from_hex as rgb
 
 from remoteCtrlServer.httpserver import start_server_in_thread
 from remoteCtrlServer.udpService import UdpAsyncClient
 
-
+from math import sin, cos, pi
 from backgroundServices.backgroundProcessor import BackgroundWorker
+from garden.graph import MyGraph
+from garden.gauge import Gauge
+
 
 
 class SystemParamObject:
@@ -113,12 +117,16 @@ class MyActionBar(ActionBar):
             self.mode_color = [1, 0, 0, 1]  # Червоний
 
 
+
+
+
 class MainScreen(FloatLayout):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.SystemParam = SystemParamObject("System", "Default")
         self.backProc = BackgroundWorker()
         self.backProc.startProc()
+        self.new_x = 0
         
         
         self.background_image = Image(source='images/bg_d.jpg', size=self.size)
@@ -148,15 +156,38 @@ class MainScreen(FloatLayout):
         self.add_widget(self.servReport)
 
 
-        self.button1 = Button(text='', size_hint=(None, None), size=(192, 192), pos_hint={'center_x': .3, 'center_y': .5},
-                              background_normal='images/switch3_off.png', background_down='images/switch3.png')
-        self.button1.bind(on_release=lambda instance: self.on_button_release("spn:dune:1"))
-        self.add_widget(self.button1)
+        #self.button1 = Button(text='', size_hint=(None, None), size=(192, 192), pos_hint={'center_x': .3, 'center_y': .5},
+        #                      background_normal='images/switch3_off.png', background_down='images/switch3.png')
+        #self.button1.bind(on_release=lambda instance: self.on_button_release("spn:dune:1"))
+        #self.add_widget(self.button1)
 
-        self.button2 = Button(text='', size_hint=(None, None), size=(192, 192), pos_hint={'center_x': .7, 'center_y': .5},
-                              background_normal='images/switch3_off.png', background_down='images/switch3.png')
-        self.button2.bind(on_release=lambda instance: self.on_button_release("spn:dune:0"))
-        self.add_widget(self.button2)
+        #self.button2 = Button(text='', size_hint=(None, None), size=(192, 192), pos_hint={'center_x': .7, 'center_y': .5},
+        #                      background_normal='images/switch3_off.png', background_down='images/switch3.png')
+        #self.button2.bind(on_release=lambda instance: self.on_button_release("spn:dune:0"))
+        #self.add_widget(self.button2)
+
+        self.dashGroup = BoxLayout(orientation='horizontal', size_hint=(None, None),  pos=(100, 100))
+
+        self.graph1 = MyGraph()
+        self.dashGroup.add_widget(self.graph1)
+        
+        self.voltmeter = Gauge(value=0, size_gauge=240, size_text=14, meterType="Voltmeter", units='V')
+        self.socmeter = Gauge(value=0, size_gauge=240, size_text=14, meterType="SOC", units='%')
+        self.tempmeter = Gauge(value=0, size_gauge=240, size_text=14, meterType="Temp", units='°C')
+
+        self.analogMetersGroup = BoxLayout(orientation='vertical' , size_hint=(None, None), size=(200, 530))
+        self.analogMetersGroup.add_widget(self.voltmeter)
+        self.analogMetersGroup.add_widget(self.socmeter)
+        self.analogMetersGroup.add_widget(self.tempmeter)
+        
+
+        self.dashGroup.add_widget(self.analogMetersGroup)
+        
+        self.add_widget(self.dashGroup)
+        
+
+
+
 
     def on_button_release(self, message):
         self.udpClient.send_data(message, udpReportService.ip, udpReportService.tx_port)
@@ -166,6 +197,7 @@ class MainScreen(FloatLayout):
     def update_time(self):
         #print(self.gpio.pinRead(self.jigSw))
         
+     
 
         self.clock.text='[color=0099ff]'+datetime.now().strftime('%H:%M:%S')+'[/color]'
         self.SystemParam.systime = datetime.now().strftime('%H:%M:%S')
@@ -180,6 +212,12 @@ class MainScreen(FloatLayout):
             json_data = json.loads(data)
             gtaSpd = data
             self.servReport.text = f"[color=00ffcc]{json_data['socStatusLoad'][0]} %, {json_data['socVoltage']/100} V, {json_data['socTemperature']/10} °C[/color]"
+            self.graph1.add_y_point(json_data['socCurrent'] / 10)
+            self.voltmeter.value = json_data['socVoltage'] / 100
+            self.socmeter.value = json_data['socStatusLoad'][0]
+            self.tempmeter.value = json_data['socTemperature'] / 10
+    
+            #self.servReport.text = f"[color=00ffcc]{gtaSpd[0]} km/h, {gtaSpd[1]} V, {gtaSpd[2]} °C[/color]"
             print(json_data)
             pass
         except:
